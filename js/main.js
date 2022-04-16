3259a20b1b20b4fab5a39f8bf16f5e8b6c951d4c
fixelmiks <malkvaevsamat@gmail.com>
2022-04-16 17:50:16 +0600

    Добавление бургер-меню

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const headerBurger = document.querySelector('.header-burger');
+
+headerBurger.addEventListener("click", () => {
+  body.classList.toggle('scroll-disabled');
+  header.classList.toggle('is-open');
+});
+
